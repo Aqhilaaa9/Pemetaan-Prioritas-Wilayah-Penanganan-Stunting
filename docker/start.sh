2826b64db8a5bdb37a2069cd4f3f62e238d66ed5
Aniiiii <rizani4366@gmail.com>
2026-09-15 16:50:07 +0800

    fix: update exposed port from 8000 to 8080 in Dockerfile and start script

--- docker/start.sh
+++ docker/start.sh
@@ -6,4 +6,4 @@ php artisan view:cache
 php artisan migrate --force
 php artisan storage:link || true
 
-exec php artisan serve --host=0.0.0.0 --port="${PORT:-8000}"
+exec php artisan serve --host=0.0.0.0 --port="${PORT:-8080}"
